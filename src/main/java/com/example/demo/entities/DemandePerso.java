c5ca41e02be2240e626624fbdaf9461aba9e55d5
unknown <galinier.arnaud@hotmail.fr>
2023-01-23 10:31:26 +0100

    modif demande perso

--- src/main/java/com/example/demo/entities/DemandePerso.java
+++ src/main/java/com/example/demo/entities/DemandePerso.java
@@ -9,8 +9,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @Data
@@ -20,17 +23,27 @@ public class DemandePerso {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NonNull
 	private Long id;
+	@NonNull
 	private String nom;
+	@NonNull
 	private String prenom;
+	@NonNull
 	private String entreprise;
+	@NonNull
 	private String telephone;
+	@NonNull
 	@Column(name = "eligible_cpf")
-	private boolean eligibleCPF;
+	private String eligibleCPF;
+	@NonNull
 	private String besoins;
+	@NonNull
 	private String objectifs;
+	
 
 	@ManyToOne
+	@JsonIgnore
 	@JoinColumn(name = "id_formation", referencedColumnName = "id")
 	private Formation formation;
 
