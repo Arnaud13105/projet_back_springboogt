63121f8d03ce368896f301c787583218f7b4c346
unknown <galinier.arnaud@hotmail.fr>
2023-01-23 13:06:44 +0100

    completion formation

--- src/main/java/com/example/demo/repository/FormationRepository.java
+++ src/main/java/com/example/demo/repository/FormationRepository.java
@@ -1,9 +1,19 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Formation;
 
-public interface FormationRepository extends JpaRepository<Formation, Long>{
+public interface FormationRepository extends JpaRepository<Formation, Long> {
+
+	List<Formation> findByNomFormation(String nomFormation);
+
+	List<Formation> findByDomaine(String domaine);
+
+	List<Formation> findByTheme(String theme);
+
+	List<Formation> findByPrix(String prix);
 
 }
--- src/main/java/com/example/demo/services/impl/FormationServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/FormationServiceImpl.java
@@ -1,12 +1,72 @@
 package com.example.demo.services.impl;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service("formationService")
-public class FormationServiceImpl {
+import com.example.demo.entities.Formation;
+import com.example.demo.repository.FormationRepository;
+import com.example.demo.services.IFormationService;
 
-	public FormationServiceImpl() {
-		// TODO Auto-generated constructor stub
+@Service("formationService")
+public class FormationServiceImpl implements IFormationService{
+	
+	@Autowired
+	private FormationRepository formationRepository;
+
+	@Override
+	public List<Formation> findAll() {
+		
+		return formationRepository.findAll();
 	}
 
+	@Override
+	public Formation saveOrUpdate(Formation o) {
+
+		return formationRepository.save(o);
+	}
+
+	@Override
+	public Optional<Formation> findById(long id) {
+		// TODO Auto-generated method stub
+		return formationRepository.findById(id);
+	}
+
+	@Override
+	public boolean delete(long id) {
+		formationRepository.deleteById(id);		
+		return true;
+	}
+
+	@Override
+	public List<Formation> findByNomFormation(String nomFormation) {
+		// TODO Auto-generated method stub
+		return formationRepository.findByNomFormation(nomFormation);
+	}
+
+	@Override
+	public List<Formation> findByDomaine(String domaine) {
+		// TODO Auto-generated method stub
+		return formationRepository.findByDomaine(domaine);
+	}
+
+	@Override
+	public List<Formation> findByTheme(String theme) {
+		// TODO Auto-generated method stub
+		return formationRepository.findByTheme(theme);
+	}
+
+	@Override
+	public List<Formation> findByPrix(String prix) {
+		// TODO Auto-generated method stub
+		return formationRepository.findByPrix(prix);
+	}
+
+
+	
 }
+
+
+
