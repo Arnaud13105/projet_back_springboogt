e631adeb662958e30e12304a47334553afed4c51
unknown <galinier.arnaud@hotmail.fr>
2023-01-24 10:00:11 +0100

    intra

--- src/main/java/com/example/demo/controllers/FormationRestController.java
+++ src/main/java/com/example/demo/controllers/FormationRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -20,6 +21,7 @@ import com.example.demo.services.IFormationService;
 
 @RestController
 @CrossOrigin("*")
+@RequestMapping("/api")
 public class FormationRestController {
 
 	@Autowired
